0e6354fbc4b6d427512b9f1d14e2e8952117fcfd
Accurics <admin@accurics.com>
2020-10-17 01:51:14 +0000

    Auto Remediation By Accurics

--- elasticache.tf
+++ elasticache.tf
@@ -14,6 +14,6 @@ resource "aws_elasticache_cluster" "redis_version_non_compliant" {
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.0"
+  engine_version       = "4.0.10"
   port                 = 6379
 }
