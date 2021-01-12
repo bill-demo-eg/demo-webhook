81246a0cf4c5f33ea4f115b5cf75a8035ff7d80e
bill-demo-eg <62158480+bill-demo-eg@users.noreply.github.com>
2021-01-12 00:32:56 -0800

    Update web.tf

--- web.tf
+++ web.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "acme_web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
 
