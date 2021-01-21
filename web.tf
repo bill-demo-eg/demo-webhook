d6ceee092a06961e0e1bf82e79d5db14cfdc6d09
Accurics <admin@accurics.com>
2021-01-20 17:15:41 -0800

    Auto Remediation By Accurics

--- web.tf
+++ web.tf
@@ -19,15 +19,15 @@ resource "aws_security_group" "acme_web" {
   }
 
 
- # HTTP access from the VPC - changed
+  # HTTP access from the VPC - changed
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["<cidr>"]
   }
 
- /* ingress {
+  /* ingress {
     to_port     = 3306
     from_port   = 3306
     protocol    = "tcp"
