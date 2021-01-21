80aa1e0101aed43c44674ac53eea6db5b27635c7
Accurics <admin@accurics.com>
2021-01-21 18:55:57 +0000

    Auto Remediation By Accurics

--- rds.tf
+++ rds.tf
@@ -1,10 +1,17 @@
 resource "aws_rds_cluster" "awsRdsNotEncrypted" {
-  master_password      = "test123"
-  master_username      = "test"
+  master_password       = "test123"
+  master_username       = "test"
+  storage_encrypted     = true
+  copy_tags_to_snapshot = true
+  deletion_protection   = true
+  backtrack_window      = 86400
 }
 
 resource "aws_rds_cluster" "storageEncryptedFalse" {
-  master_password      = "test123"
-  master_username      = "test"
-  storage_encrypted    = false
+  master_password       = "test123"
+  master_username       = "test"
+  storage_encrypted     = true
+  copy_tags_to_snapshot = true
+  deletion_protection   = true
+  backtrack_window      = 86400
 }
